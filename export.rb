a2132207f0bcd66f67d3df3086a749655933af45
flck <florian.schroeck@stud.htwk-leipzig.de>
2024-01-25 18:31:54 +0100

    Export STDERR to error_log

--- export.rb
+++ export.rb
@@ -3,23 +3,28 @@ require 'json'
 
 module Export
 
-  def export_data (data, filename, export_dir, timestamp)
-
-    # create directories
-    export_dir_path = Pathname.new(export_dir)
-    timestamp_subdir_path = export_dir_path.join(timestamp)
-
-    export_dir_path.mkdir unless export_dir_path.exist?
-    timestamp_subdir_path.mkdir unless timestamp_subdir_path.exist?
+  def export_data (data, filename, export_dir)
 
     # JSON export
-    File.open(timestamp_subdir_path.join(filename + ".json"), 'w') do |file|
+    File.open(export_dir.join(filename + ".json"), 'w') do |file|
       file.write(JSON.pretty_generate(data))
     end
 
     # Binary export
-    File.open(timestamp_subdir_path.join(filename), 'wb') do |file|
+    File.open(export_dir.join(filename), 'wb') do |file|
       file.write(Marshal.dump(data))
     end
+  end
+
+
+  def create_directories(export_dir,timestamp)
+    # create directories
+    export_dir_path = Pathname.new(export_dir)
+    export_dir_with_timestamp_path = export_dir_path.join(timestamp)
+
+    export_dir_path.mkdir unless export_dir_path.exist?
+    export_dir_with_timestamp_path.mkdir unless export_dir_with_timestamp_path.exist?
+
+    export_dir_with_timestamp_path
   end
 end
--- rubycrypt.rb
+++ rubycrypt.rb
@@ -3,17 +3,13 @@
 require_relative 'reverse_dependencies'
 require_relative 'traverse'
 require_relative 'export'
+include Export
 
 
-
+SCAN_DIRECTORY = Dir.pwd # current directory
+EXPORT_DIR = "/Daten/Code/RubyCrypt/scans/"
+CRYPTO_GEMS = %w[openssl bcrypt attr_encrypted lockbox rbnacl scrypt strongox cose encryptor digest net-ssh ssh_data sshkey]
 class Analyzer
-  include Export
-
-  SCAN_DIRECTORY = Dir.pwd # current directory
-  EXPORT_DIR = "/Daten/Code/RubyCrypt/scans/"
-  CRYPTO_GEMS = %w[openssl bcrypt attr_jncrypted lockbox rbnacl scrypt strongox cose encryptor digest]
-
-  attr_reader :scan_dir, :export_dir, :gems
 
   def initialize(scan_dir = SCAN_DIRECTORY, export_dir = EXPORT_DIR, *gems)
 
@@ -22,11 +18,15 @@ class Analyzer
     gems.empty? ? @gems = CRYPTO_GEMS : @gems = gems
   end
 
+  attr_reader :scan_dir, :export_dir, :gems
   def analyze(scan_dir = @scan_dir, gems = @gems)
 
     # calculate chains of reverse dependencies
     dependency_chains = why(gems)
-    relevant_gems = Regexp.union(dependency_chains.inject(:+).uniq) unless dependency_chains.empty?
+
+    # RegExp to search for require statements
+    # gem naming convention (see https://guides.rubygems.org/name-your-gem/): "-" in the gem name (e.g. net-ssh) turn into "/" in the require statement (require net/ssh)
+    relevant_gems = Regexp.union(dependency_chains.inject(:+).uniq.map {|gem| gem.gsub("-", "/")}) unless dependency_chains.empty?
 
     # extract requirements from Ruby files in scan_dir & subdirectories
     requirements, errors = traverse(scan_dir)
@@ -35,9 +35,11 @@ class Analyzer
 
 
     # Export
-    timestamp = Time.now.strftime('%Y-%m-%dT%H-%M-%S')
-
     data_to_export = {
+      "config" => {
+        "SCAN_DIR" => scan_dir,
+        "CRYPTO_GEMS" => gems
+      },
       "dependency_chains" => dependency_chains,
       "requirements" => requirements,
       "filtered_requirements" => filtered_requirements,
@@ -45,11 +47,40 @@ class Analyzer
     }
 
     data_to_export.each do |filename, data|
-      export_data(data, filename, @export_dir, timestamp)
+      export_data(data, filename, @export_dir)
     end
 
   end
 end
 
-analyzer = Analyzer.new(*ARGV)
+# Parse Command Line Arguments
+# TODO Use OptionParser for proper handling of Command Line Arguments
+case ARGV.size
+when 0
+  scan_dir = SCAN_DIRECTORY
+  export_dir = EXPORT_DIR
+  gems = CRYPTO_GEMS
+when 1
+  scan_dir = ARGV[0]
+  export_dir = EXPORT_DIR
+  gems = CRYPTO_GEMS
+when 2
+  scan_dir = ARGV[0]
+  export_dir = ARGV[1]
+  gems = CRYPTO_GEMS
+else
+  scan_dir = ARGV[0]
+  export_dir = ARGV[1]
+  gems = ARGV[2..ARGV.size]
+end
+
+# Create export directory with timestamp
+export_dir_with_timestamp = Export.create_directories(export_dir,Time.now.strftime('%Y-%m-%dT%H-%M-%S'))
+analyzer = Analyzer.new(scan_dir, export_dir_with_timestamp, *gems)
+
+# redirect stderr to error_log file
+original_stderr = $stderr.dup
+$stderr.reopen(export_dir_with_timestamp.join('error_log').to_s, 'w')
+$stderr.sync = true
 analyzer.analyze
+$stderr.reopen(original_stderr)
